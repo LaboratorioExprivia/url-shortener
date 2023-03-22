3c61cdcaa6f85080f99b15b8e31b1736756752db
LaboratorioExprivia <118175982+LaboratorioExprivia@users.noreply.github.com>
2023-03-22 14:32:11 +0000

    Update UrlShortenerApplication.java

--- src/main/java/com/github/PopovDmitry/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/github/PopovDmitry/urlshortener/UrlShortenerApplication.java
@@ -3,11 +3,17 @@ package com.github.PopovDmitry.urlshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class UrlShortenerApplication {
+public class UrlShortenerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
-	}
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(serverTestApplication.class);
+    }
 }
